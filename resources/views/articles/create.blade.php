0308b2698e75029b88dd209781e16b63ec2b0e83
khmalz <akmalrma1710@gmail.com>
2023-03-13 15:20:29 +0700

    chore: add javascript line for select library

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -60,6 +60,13 @@
     </div>
 
     <x-slot name="js">
+        <script src="https://code.jquery.com/jquery-3.6.4.min.js"
+            integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('.multiple').select2();
+            });
+        </script>
     </x-slot>
 </x-app-layout>
